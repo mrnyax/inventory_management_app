888d13f8ac3dcd92d0a4899ab66c84905dc0ac45
Patrick Mogusu <nyakoria@Patricks-MacBook-Pro.local>
2025-04-11 21:00:15 +0300

    Initial project setup
    # Conflicts:
    #	.gitignore

---
+++ android/app/src/main/java/com/invento/app/inventory_management_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.invento.app.inventory_management_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/invento/app/inventory_management_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.invento.app.inventory_management_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
